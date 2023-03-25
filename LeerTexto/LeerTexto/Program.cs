05051b1d23d5a07d750272d7441800181517a275
Carlos Hernandez <cralitos@gmail.com>
2023-03-24 23:39:37 -0600

    archivos

---
+++ LeerTexto/LeerTexto/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LeerTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metodo 1
+            string[] lineas = { "Primero 250", "Segundo 242", "Tercero 240" };
+
+            File.WriteAllLines(@"C:\Users\Win10\Desktop\Textos\Puntajes.txt", lineas);
+
+
+            /*
+            //Metodo 2
+            Console.WriteLine("Por favor ingrese el nombre del archivo");
+            string nombreArchivo = Console.ReadLine();
+            Console.WriteLine("Por favor ingrese el contenido del archivo");
+            string texto = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\Win10\Desktop\Textos\" + nombreArchivo + ".txt", texto);
+            */
+
+            //Metodo 3
+            using(StreamWriter archivo = new StreamWriter (@"C:\Users\Win10\Desktop\Textos\miTexto2.txt"))
+            {
+                foreach(string linea in lineas)
+                {
+                    if(linea.Contains("4"))
+                    {
+                        archivo.WriteLine(linea);
+                    }
+                }
+            }
+
+            using (StreamWriter archivo = new StreamWriter (@"C:\Users\Win10\Desktop\Textos\miTexto2.txt", true))
+            {
+                archivo.WriteLine("Linea adicional");
+            }
+
+
+            /*//Leer texto completo
+            string texto = System.IO.File.ReadAllText(@"C:\Users\Win10\Desktop\Textos\LeerArchivoTexto.txt");
+            Console.WriteLine("El archivo contiene el siguiente texto: {0}", texto);
+
+            //Leear de a líneas
+            string[] lineas = System.IO.File.ReadAllLines(@"C:\Users\Win10\Desktop\Textos\LeerArchivoTexto.txt");
+            foreach(string linea in lineas)
+            {
+                Console.WriteLine("\t" + linea);
+            }
+            */
+            Console.Read();
+        }
+    }
+}
