e36cc5e31e0e142c8615be5b6fde600c50e3da88
Carlos Hernandez <cralitos@gmail.com>
2023-03-24 23:35:18 -0600

    first commit

---
+++ Decisiones/if_anidado/TomarDecisiones/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TomarDecisiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool esAdmin = false;
+            bool estaRegistrado = true;
+            string nombreDeUsuario = "";
+
+            Console.WriteLine("Por favor, ingrese su nombre de usuario");
+            nombreDeUsuario = Console.ReadLine();
+            if (estaRegistrado)
+            {
+                Console.WriteLine("Hola usuario registrado");
+                if (nombreDeUsuario != "")
+                {
+                    Console.WriteLine("Hola " + nombreDeUsuario);
+                    if (nombreDeUsuario.Equals("Admin"))
+                    {
+                        Console.WriteLine("Hola Admin");
+                    }
+                }
+            }
+
+            if(estaRegistrado  && nombreDeUsuario != "" && nombreDeUsuario.Equals("Admin"))
+            {
+                Console.WriteLine("Hola usuario registrado");
+                Console.WriteLine("Hola " + nombreDeUsuario);
+                Console.WriteLine("Hola Admin");
+            }
+
+            if(esAdmin || estaRegistrado)
+            {
+                Console.WriteLine("Estás loguado");
+            }
+
+
+            Console.Read();
+        }
+       
+    }
+}
